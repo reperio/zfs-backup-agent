5fdef4bf35c159b9df138542c8d924abdde7eaf6
Brad Gardner <bradgardner@sevenhillstechnology.com>
2017-10-21 19:33:18 -0400

    REP-11 updating logging and added command logging, refactored to add listeners for every child process event and output to logs

--- api/handlers/zfs_agent.js
+++ api/handlers/zfs_agent.js
@@ -21,17 +21,21 @@ routes.push({
     }
 });
 async function create_snapshot(request, reply) {
+    const logger = request.server.app.logger;
+
     try {
+        const payload = JSON.stringify(request.payload);
+        logger.info(`create_snapshot called with payload: ${payload}`);
+
         const snapshot_name = request.payload.snapshot_name;
 
-        request.server.app.logger.info(`Creating snapshot: ${snapshot_name}`);
+        logger.info(`Creating snapshot: ${snapshot_name}`);
 
-        const api = new zfs_api();
+        const api = new zfs_api(logger);
 
         const status_code = await api.create_snapshot(snapshot_name);
 
-        request.server.app.logger.info(`Create snapshot finished with code: ${status_code}`);
-        
+        logger.info(`Create snapshot finished with code: ${status_code}`);
 
         return reply({message:'success', status_code: status_code});
     }
@@ -63,29 +67,34 @@ routes.push({
     }
 });
 async function send_snapshot(request, reply) {
+    const logger = request.server.app.logger;
+
     try {
+        const payload = JSON.stringify(request.payload);
+        logger.info(`send_snapshot called with payload: ${payload}`);
+
         const snapshot_name = request.payload.snapshot_name;
         const host = request.payload.host;
         const port = request.payload.port;
         const incremental = request.payload.incremental;
         const source_snapshot_name = request.payload.source_snapshot_name;
 
-        request.server.app.logger.info(`Sending snapshot: ${snapshot_name} to ${host}:${port}`);
+        logger.info(`Sending snapshot: ${snapshot_name} to ${host}:${port}`);
 
-        const api = new zfs_api();
+        const api = new zfs_api(logger);
 
         api.send_mbuffer_to_host(snapshot_name, host, port, incremental, source_snapshot_name).then(function(code) {
             request.server.app.logger.info(`Send snapshot finished with code: ${code}`);
         }).catch(function(code) {
-            request.server.app.logger.error(`Send snapshot finished with code: ${code}`);
+            request.server.app.logger.error(`Send snapshot failed with error: ${code}`);
         });
 
-        request.server.app.logger.info(`Send command executed.`);
+        logger.info(`Send command executed.`);
 
         return reply({message:'success', status_code: 0});
     }
     catch (e) {
-        request.server.app.logger.error(e);
+        logger.error(e);
 
         return reply(Boom.badImplementation('Snapshot send failed.'));
     }
@@ -106,24 +115,29 @@ routes.push({
     }
 });
 async function receive_snapshot(request, reply) {
+    const logger = request.server.app.logger;
+
     try {
+        const payload = JSON.stringify(request.payload);
+        logger.info(`receive_snapshot called with payload: ${payload}`);
+
         const target = request.payload.target;
         const port = request.payload.port;
 
-        request.server.app.logger.info(`Receiving snapshot: ${target} on port ${port}`);
+        logger.info(`Receiving snapshot: ${target} on port ${port}`);
 
-        const api = new zfs_api();
+        const api = new zfs_api(logger);
 
         api.receive_mbuffer_to_zfs_receive(target, port).then(function(code) {
-            request.server.app.logger.info(`Receive snapshot finished with code: ${code}`);
+            logger.info(`Receive snapshot finished with code: ${code}`);
         }).catch(function(code) {
-            request.server.app.logger.error(`Receive snapshot finished with code: ${code}`);
+            logger.error(`Receive snapshot finished with code: ${code}`);
         });
 
         return reply({message:'success', status_code: 0});
     }
     catch (e) {
-        request.server.app.logger.error(e);
+        logger.error(e);
 
         return reply(Boom.badImplementation('Snapshot receive failed.'));
     }
