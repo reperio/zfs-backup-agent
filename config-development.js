340bc5900871d240dbf7a48cc9584fe6ada1422c
Brad Gardner <bradgardner@sevenhillstechnology.com>
2017-11-12 18:21:29 -0500

    agent config

--- config-development.js
+++ config-development.js
@@ -1,11 +1,11 @@
 module.exports = {
-	log_directory: './logs',
-	trace_log_level: 'info',
-	app_file_log_level: 'info',
-	app_json_log_level: 'info',
-	host: '0.0.0.0',
-	port: 8000,
-	mbuffer_size: '1G',
-	mbuffer_rate: '500k',
-	controller_api_url: 'http://localhost:3000/api'
+    log_directory: './logs',
+    trace_log_level: 'info',
+    app_file_log_level: 'info',
+    app_json_log_level: 'info',
+    host: '0.0.0.0',
+    port: 3000,
+    mbuffer_size: '1G',
+    mbuffer_rate: '500k',
+    controller_api_url: 'http://192.168.156.1:3000/api'
 };
